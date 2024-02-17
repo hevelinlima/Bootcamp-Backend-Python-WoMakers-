fe8dd1e9a00d6f69aa12658fd6d903c8c97062e9
hevelinlima <hevelin25@gmail.com>
2024-02-17 01:04:53 -0300

    semana 03

---
+++ semana-03/POO/modelando-um-sistema.py
@@ -0,0 +1,9 @@
+"""from bancoDelas import *
+
+info_conta=ContaCorrente()
+
+info_conta.adc_titular('Maria José')
+info_conta.adc_titular('Sávio')
+info_conta.adc_titular('Hemilly')
+
+info_conta.listar_titulares()"""
